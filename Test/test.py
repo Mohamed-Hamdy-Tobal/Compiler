c07f49098a8eff45ce69babef8b6c6f03480a6a6
DESKTOP-F42UCN8\7azem_store <mohamed.tolba@lastcloud.com.au>
2024-05-15 23:08:43 +0300

    Upload

--- Test/test.py
+++ Test/test.py
@@ -1,46 +1,45 @@
 import re
-
 TOKEN_TYPES = [
-    ("NUMBER", r"\d+"),
-    ("STRING", r'"[^"]*"'),
-    ("VARIABLE", r"[a-zA-Z_][a-zA-Z0-9_]*"),
-    ("ASSIGN", r"="),
-    ("PLUS", r"\+"),
-    ("MINUS", r"-"),
-    ("MULTIPLY", r"\*"),
-    ("DIVIDE", r"/"),
-    ("LPAREN", r"\("),
-    ("RPAREN", r"\)"),
-    ("WHITESPACE", r"\s+"),
+    ("NUMBER", r'\d+(\.\d+)?'),
+    ("PLUS", r'\+'),
+    ("MINUS", r'\-'),
+    ("MULTIPLY", r'\*'),
+    ("DIVIDE", r'\/'),
+    ("ASSIGN", r'\='),
+    ("LPAREN", r'\('),
+    ("RPAREN", r'\)'),
+    ("VARIABLE", r'[a-zA-Z_][a-zA-Z0-9_]*'),
+    ("STRING", r'\".*?\"'),
+    ("WHITESPACE", r'\s+'),
+    ("UNKNOWN", r'\.'),
 ]
 
-TOKEN_REGEX = [(name, re.compile(pattern)) for name, pattern in TOKEN_TYPES]
+TOKENS_REGEX = [(name, re.compile(pattern)) for name, pattern in TOKEN_TYPES]
 
 def lexer(file_path):
     tokens = []
     with open(file_path, 'r') as file:
-        for line_number, line in enumerate(file, 1):
+        for line_num, line in enumerate(file, start=1):
             line_token = []
             line = line.strip()
             while line:
                 match = None
-                for token_name, token_regex in TOKEN_REGEX:
+                for token_name, token_regex in TOKENS_REGEX:
                     regex_match = token_regex.match(line)
                     if regex_match:
                         match = (token_name, regex_match.group(0))
                         if match[0] != "WHITESPACE":
                             line_token.append(match)
-                            # line_token.append((match[0], match[1], f"In -> {line_number}"))
                         line = line[regex_match.end() :]
                         break
                 if not match:
-                    raise ValueError("Error")
+                    raise ValueError("error")
             tokens.append(line_token)
     return tokens
 
 
 class OrderedTable:
-    def __init__(self):
+    def __init__(self) -> None:
         self.table = []
 
     def insert(self, name, datatype, line_declare):
@@ -49,45 +48,43 @@ class OrderedTable:
     def lookup(self, name):
         for i in self.table:
             if i[0] == name:
-                return i[1]  # Return the value associated with the name
-        return None  # Return None if the name is not found
-
+                return i[1]
+        return None
+    
     def sorted_table(self):
-        return sorted(self.table, key=lambda x: x[0])  # Sort the table alphabetically by variable name
-
-
+        return sorted(self.table, key=lambda x: x[0])
+    
 
 class Parser:
-    def __init__(self, tokens, table):
+    def __init__(self, tokens, table) -> None:
+        self.table = table
         self.tokens = tokens
         self.current_token_index = 0
-        self.table = table # parameter refers to the symbol table where variable declarations are stored.
 
     def parse(self):
         return self.assignment()
-
+    
     def assignment(self):
         variable_name_token = self.consume("VARIABLE")
         self.consume("ASSIGN")
         expression_value = self.expression()
 
-        # For Table
         value = expression_value if isinstance(expression_value, str) else expression_value[2]
         datatype = "float" if "." in value else "int" if value.replace(".", "").isdigit() else "str"
         if not self.table.lookup(variable_name_token[1]):
             self.table.insert(variable_name_token[1], datatype, self.current_line)
-        
-        return ("=", variable_name_token[1], value)
 
+        return ("=", variable_name_token[1], value)
+    
     def factor(self):
-        token_type, token_value = self.consume("NUMBER", "STRING", "VARIABLE", "LPAREN")
-        if token_type == "NUMBER" or token_type == "STRING" or token_type == "VARIABLE":
+        token_type, token_value = self.consume("NUMBER", "VARIABLE", "STRING", "LPAREN")
+        if token_type == "NUMBER" or token_type == "VARIABLE" or token_type == "STRING" :
             return token_value
-        elif token_type == "LPAREN":
+        elif token_type == "LPAREN" :
             expression_value = self.expression()
             self.consume("RPAREN")
             return expression_value
-
+        
     def term(self):
         result = self.factor()
         while self.current_token_index < len(self.tokens):
@@ -97,7 +94,7 @@ class Parser:
             else:
                 break
         return result
-
+    
     def expression(self):
         result = self.term()
         while self.current_token_index < len(self.tokens):
@@ -116,7 +113,7 @@ class Parser:
             raise ValueError(f"Expected one of {expected_token_types}, got {current_token}")
         self.current_token_index += 1
         return current_token
-
+    
 
 table = OrderedTable()
 file_path = 'text1.txt'
@@ -125,9 +122,6 @@ tokens = lexer(file_path)
 print('Parse Tree : ')
 for line_num, line_tokens in enumerate(tokens, start=1):
     parser = Parser(line_tokens, table)
-    # print(f"\nParse For Line {line_num} : ")
-    # for token in line_tokens:
-    #     print(token)
     parser.current_line = line_num  # Track the line number where the variable was declared
     parse_tree = parser.parse()
     print(parse_tree)
@@ -151,3 +145,4 @@ for count, entry in enumerate(table.sorted_table(), start=1):
     line_repeat_str = ', '.join(map(str, line_repeat)) if line_repeat else []
     
     print(f"{count:<7} | {variable_name:<8} | {datatype:<9} | {line_declare:<12} | {line_repeat_str}")
+
